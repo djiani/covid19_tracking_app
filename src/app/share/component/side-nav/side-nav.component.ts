e30d3cf9bbfb950eb05a5fac386eff73259690e6
Uncle Sam <djiasara@gmail.com>
2020-04-14 16:30:04 -0400

    display worldwide covid19 statics

--- src/app/share/component/side-nav/side-nav.component.ts
+++ src/app/share/component/side-nav/side-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Covid19DataService } from 'src/app/Covid19Service/covid19Data.service';
 
+
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
@@ -8,15 +9,21 @@ import { Covid19DataService } from 'src/app/Covid19Service/covid19Data.service';
 })
 export class SideNavComponent implements OnInit {
 
-  data:any;
-  today: number = Date.now();
+  covid19Data:any;
+  diff: number = Date.now();
+
   constructor(private covid19DataService: Covid19DataService ) { }
 
   ngOnInit() {
     this.covid19DataService.getStatsWorldWide()
     .subscribe( data => {
       console.log(data);
-      data = data;
+      this.covid19Data = data;
+      const updated = new Date(this.covid19Data.updated);
+      const today = new Date();
+      this.diff = today.getTime() - updated.getTime();
+      console.log(' up ', today.getTime());
+      console.log(' diff ', this.diff);
     });
 
   }
